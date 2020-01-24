85897039247eed1bbae3716f96eb4cf1ae285b5a
Brian Price <brianprice88@gmail.com>
2020-01-23 16:21:43 -0800

    "Connected front end to server."

---
+++ server/index.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+const express = require('express');
+
+const app = express();
+const port = 3003;
+
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+// const router = require('router.js');
+// const db = require('../db)
+
+// middleware
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+// app.use('/api', router);
+
+app.use('/', (req, res) => res.send('hello world'));
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
