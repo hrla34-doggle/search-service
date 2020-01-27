7b96a9433c6db0f27880bb03ece1d520f266281f
Brian Price <brianprice88@gmail.com>
2020-01-26 22:05:03 -0800

    "Add getAllTrips function to retrieve relevant search data from all trips in db when page first loads."

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,14 +3,38 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import ReactDom, { render } from 'react-dom';
+import Axios from 'axios'
 
 export default class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      allTrips : [],
+      currentTrip : [],
+      search : ''
     };
+this.getAllTrips = this.getAllTrips.bind(this);
+
   }
+
+getAllTrips () {
+  // initial request for when the page first loads
+  // to retrieve the name/season/year/id for each trip
+  // store as allTrips in this.state
+  // use name/season/year for search results dropdown
+  // and id of a selected trip to retrieve the complete trip info
+  Axios.get('/api/trips')
+  .then (trips => this.setState({
+    allTrips: trips.data
+  }), console.log(this.state.allTrips))
+  .catch (err => console.error(err))
+}
+
+componentDidMount () {
+  this.getAllTrips();
+}
+
+
 
   render() {
     return (
---
+++ db/aws-config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+
+const config = {
+
+  AWSAccessKeyId: 'AKIAJLBHXIZ2V4LHYENA',
+
+  AWSSecretKey: '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',
+
+};
+
+module.exports = config;
