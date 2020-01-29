b71bd58a3f2dde833da30b1bb6e6072eee06e840
Brian Price <brianprice88@gmail.com>
2020-01-28 19:41:47 -0800

    "Add magnifying glass which will act as search button next to bar"

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -9,13 +9,30 @@ const Search = (props) => {
     paddingLeft: '16px',
     boxSizing: 'border-box',
     height: '44px',
+    marginBottom: '10px',
     border: '1px solid #d6d6d6',
     boxShadow: '0 4px 4px -4px #767676',
     background: '#fff',
   };
+  const imgStyle = {
+    height: '44px',
+    width: '44px',
+    border: '1px solid gray',
+    display: 'inline-block',
+  };
   return (
     <div>
-      <input style={barStyle} type="text" placeholder="Where to or what trip?" />
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              {' '}
+              <input id="searchBar" style={barStyle} type="text" placeholder="Where to or what trip?" />
+            </td>
+            <td><img id="magnifyingGlass" style={imgStyle} src="https://cdn1.iconfinder.com/data/icons/utility/100/SVG_Utility-07-512.png" alt="magnifying glass" /></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
