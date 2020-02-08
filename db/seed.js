48129b55f5a211a773ecd93fc78cc06ef7db6ff6
Brian Price <brianprice88@gmail.com>
2020-02-08 11:28:15 -0800

    "Change seeding function countries selection to match up with Edgar's"

--- db/seed.js
+++ db/seed.js
@@ -6,15 +6,26 @@ const db = require('./index.js');
 
 const Trip = require('./schema.js');
 
+var countries = {
+  Africa: ['Egypt', 'Kenya', 'Morocco', 'South Africa'],
+  Asia: ['China', 'Israel', 'India', 'Japan', 'South Korea', 'Thailand', 'Vietnam'],
+  Europe: ['France', 'Germany', 'Greece', 'Ireland', 'Italy', 'The Netherlands', 'Norway', 'Poland', 'Portugal', 'Russia', 'Spain', 'Switzerland', 'The United Kingdom'],
+  'North America': ['The United States', 'Costa Rica', 'Mexico', 'Canada'],
+  'South America': ['Brazil', 'Peru'],
+  'South Pacific': ['Australia', 'New Zealand'],
+};
+
+
+var locations = ['Australia', 'Israel', 'New Zealand', 'Israel', 'Australia', 'South Korea', 'South Africa', 'South Korea', 'Canada','China', 'Switzerland', 'New Zealand', 'Costa Rica', 'New Zealand', 'New Zealand', 'Costa Rica', 'Peru', 'Kenya', 'Morocco', 'Thailand', 'Kenya', 'Russia', 'Egypt', 'Morocco', 'Australia', 'China', 'New Zealand', 'Mexico', 'Costa Rica', 'Brazil', 'Poland', 'Costa Rica', 'Norway','Peru','South Korea', 'Japan', 'Mexico','The United States', 'France', 'Japan', 'Vietnam', 'Brazil', 'Ireland', 'Canada', 'Australia', 'Thailand', 'Vietnam', 'Peru', 'Egypt', 'Brazil', 'Greece', 'Costa Rica', 'Kenya', 'Kenya', 'South Korea', 'New Zealand', 'Greece', 'Mexico', 'Australia', 'Costa Rica', 'Norway', 'South Korea', 'Vietnam', 'The United States', 'Greece', 'South Africa', 'Morocco', 'India', 'Kenya', 'Australia', 'New Zealand', 'Germany', 'Peru', 'South Africa', 'Peru', 'Italy', 'Ireland', 'India', 'Australia', 'Morocco', 'The United States', 'Vietnam', 'Israel', 'Poland', 'Italy', 'Egypt', 'Mexico', 'Spain', 'Japan', 'Canada', 'Costa Rica', 'Switzerland', 'Israel', 'Peru', 'Brazil', 'Brazil', 'Japan', 'South Africa', 'Mexico', 'Poland']
 
 // function to create a trip from random data
-const tripGenerator = () => {
+const tripGenerator = (x) => {
   const trip = {};
   trip.season = data.season();
   trip.year = 2020;
   trip.days = data.days();
-  trip.continent = data.continent();
-  trip.country = data.country(trip.continent);
+  trip.country = locations[x];
+  trip.continent = ()=>{for( var continent in countries ){ if( countries.continent.includes(trip.country) ){return continent}}};
   trip.cities = data.cities(trip.country);
   trip.imageUrl = data.imageUrl(trip.cities);
   trip.name = data.name(trip.country);
@@ -43,7 +54,7 @@ const createTrips = () => {
   const names = [];
   let tripsAdded = 0;
   while (tripsAdded < 100) {
-    const newTrip = tripGenerator();
+    const newTrip = tripGenerator(tripsAdded);
     if (!names.includes(newTrip.name)) {
       trips[tripsAdded] = newTrip;
       trips[tripsAdded].id = tripsAdded + 1;
