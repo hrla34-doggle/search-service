e0cc58d6264f6f0ce3b637d9c25c7b633682a264
jhuthmacher <jhuthmacher@gmail.com>
2020-02-25 10:16:49 -0800

    Artillery and NewRelic added

--- server/pgcontrollers.js
+++ server/pgcontrollers.js
@@ -5,9 +5,10 @@ const db = require('../database/queries');
 const controllers = {
   // for initial page load, we get all trips' name/season/year for displaying in search bar, and id for if a trip is selected by user, and cities for searching by city
   findAll: (req, res) => (
-    db.getAll()
-      .then((trips) => res.status(200).send(trips.map((trip) => [trip.id, trip.name, trip.season, trip.year, trip.continent, trip.cities, trip.country])))
-      .catch((err) => res.status(400).send(err))
+    res.status(200).send('not built yet')
+    // db.getAll()
+    //   .then((trips) => res.status(200).send(trips.map((trip) => [trip.id, trip.name, trip.season, trip.year, trip.continent, trip.cities, trip.country])))
+    //   .catch((err) => res.status(400).send(err))
   ),
   // for when a user selects a trip, use its id to get all the information about that trip in order to render the page appropriately
   findOne: (req, res) => (
@@ -37,19 +38,22 @@ const controllers = {
       .catch((err) => res.status(400).send(err.stack))
   ),
   post: (req, res) => (
-    db.post(req.body)
-      .then(() => res.status(200).send(req.body.name + ' posted!'))
-      .catch((err) => res.status(400).send(err))
+    res.status(200).send('not built yet')
+    // db.post(req.body)
+    //   .then(() => res.status(200).send(req.body.name + ' posted!'))
+    //   .catch((err) => res.status(400).send(err))
   ),
   update: (req, res) => (
-    db.update(req.params.id, req.body)
-      .then(() => res.status(200).send(req.params.id + ' updated!'))
-      .catch((err) => res.status(400).send(err))
+    res.status(200).send('not built yet')
+    // db.update(req.params.id, req.body)
+    //   .then(() => res.status(200).send(req.params.id + ' updated!'))
+    //   .catch((err) => res.status(400).send(err))
   ),
   delete: (req, res) => (
-    db.delete(req.params.id)
-      .then(() => res.status(200).send(req.params.id + ' deleted!'))
-      .catch((err) => res.status(400).send(err))
+    res.status(200).send('not built yet')
+    // db.delete(req.params.id)
+    //   .then(() => res.status(200).send(req.params.id + ' deleted!'))
+    //   .catch((err) => res.status(400).send(err))
   ),
 };
 
